b3dea734fbb56afdb57b73056f2555aa54a34c63
zxum <zion.um@outlook.com>
2021-05-04 10:58:28 -0500

    Create alert box if city is not found

--- index.js
+++ index.js
@@ -14,55 +14,66 @@ let tempUnit = document.getElementById('temp-unit')
 
 
 async function getCurrentWeather(cityname, units) {
-    let currenturl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=${units}&appid=${apikey}`
+    try {
+        let currenturl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=${units}&appid=${apikey}`
 
-    let response = await fetch(currenturl, { mode: 'cors' });
-    let weatherData = await response.json();
+        let response = await fetch(currenturl, { mode: 'cors' });
+        let weatherData = await response.json();
 
-    //Render City Name 
-    renderElement(cityArea, capitalize(cityname))
+        if (!response.ok) { 
+          alert("City not found!")
+          throw new Error(response.statusText) 
+        }
+        
+        if (response.ok) {
+          //Render City Name 
+          renderElement(cityArea, capitalize(cityname))
 
-    // Condition Desc
-    let conditionDesc = weatherData.weather[0].description
-    renderElement(conditionDescArea, capitalize(conditionDesc))
+          // Condition Desc
+          let conditionDesc = weatherData.weather[0].description
+          renderElement(conditionDescArea, capitalize(conditionDesc))
 
-    // Condition Icon
-    let conditionId = weatherData.weather[0].id
-    let sunriseRaw = weatherData.sys.sunrise
-    let sunsetRaw = weatherData.sys.sunset
-    let currentDataTime = weatherData.dt
-    renderConditions(currentDataTime, sunriseRaw, sunsetRaw, conditionId)
+          // Condition Icon
+          let conditionId = weatherData.weather[0].id
+          let sunriseRaw = weatherData.sys.sunrise
+          let sunsetRaw = weatherData.sys.sunset
+          let currentDataTime = weatherData.dt
+          renderConditions(currentDataTime, sunriseRaw, sunsetRaw, conditionId)
 
-    // Temp
-    let temp = Math.round(weatherData.main.temp)
-    renderElement(tempArea, temp)
+          // Temp
+          let temp = Math.round(weatherData.main.temp)
+          renderElement(tempArea, temp)
 
-    // Humidity 
-    let humidity = weatherData.main.humidity
-    renderElement(humidityArea, humidity + "%")
+          // Humidity 
+          let humidity = weatherData.main.humidity
+          renderElement(humidityArea, humidity + "%")
 
-    // Wind Speed 
-    let windSpeed = Math.round(weatherData.wind.speed)
-    if (units === 'metric') {
-        renderElement(windspeedArea, windSpeed + 'm/s')
-        tempUnit.classList = 'wi wi-celsius'
-    } else {
-        renderElement(windspeedArea, windSpeed + 'mph')
-        tempUnit.classList = 'wi wi-fahrenheit'
+          // Wind Speed 
+          let windSpeed = Math.round(weatherData.wind.speed)
+          if (units === 'metric') {
+              renderElement(windspeedArea, windSpeed + 'm/s')
+              tempUnit.classList = 'wi wi-celsius'
+          } else {
+              renderElement(windspeedArea, windSpeed + 'mph')
+              tempUnit.classList = 'wi wi-fahrenheit'
+          }
+
+          // Wind Deg
+          let windDeg = weatherData.wind.deg
+          windDegIcon.classList.add(`towards-${windDeg}-deg`)
+
+          // Sunrise Sunset 
+          let timezone = weatherData.timezone
+          let sunrise = convertToLocationTime(weatherData.sys.sunrise, timezone)
+          renderElement(sunriseArea, sunrise)
+          let sunset = convertToLocationTime(weatherData.sys.sunset, timezone)
+          renderElement(sunsetArea, sunset)
+
+          console.log({ weatherData })
+      }
+    } catch (err) {
+        console.log(err)
     }
-
-    // Wind Deg
-    let windDeg = weatherData.wind.deg
-    windDegIcon.classList.add(`towards-${windDeg}-deg`)
-
-    // Sunrise Sunset 
-    let timezone = weatherData.timezone
-    let sunrise = convertToLocationTime(weatherData.sys.sunrise, timezone)
-    renderElement(sunriseArea, sunrise)
-    let sunset = convertToLocationTime(weatherData.sys.sunset, timezone)
-    renderElement(sunsetArea, sunset)
-
-    console.log({ weatherData })
 
 }
 
@@ -72,6 +83,7 @@ async function getCurrentWeather(cityname, units) {
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let searchInput = document.getElementById('search')
+
     let inputValue = searchInput.value.trim()
 
     getCurrentWeather(inputValue, 'imperial')
